70e66f3cefa6b52e4e78e9fe7bd0d6f8aec51646
no1Gangster <chiragmcmahon@gmail.com>
2024-06-17 17:12:58 +0530

    Setup Completed for MERN Stack and Java programming

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,42 +1,65 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    lazy = false,
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    opts = {
-      auto_install = true,
+    {
+        "williamboman/mason.nvim",
+        lazy = false,
+        config = function()
+            require("mason").setup()
+        end,
     },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    lazy = false,
-    config = function()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+    {
+        "williamboman/mason-lspconfig.nvim",
+        lazy = false,
+        opts = {
+            auto_install = true,
+        },
+    },
+    {
+        "nvim-lua/completion-nvim",
+        config = function()
+            require("completion").on_attach()
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        lazy = false,
+        config = function()
+            local capabilities = vim.lsp.protocol.make_client_capabilities()
+            capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-      local lspconfig = require("lspconfig")
-      lspconfig.tsserver.setup({
-        capabilities = capabilities
-      })
-      lspconfig.solargraph.setup({
-        capabilities = capabilities
-      })
-      lspconfig.html.setup({
-        capabilities = capabilities
-      })
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities
-      })
+            local lspconfig = require("lspconfig")
 
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-    end,
-  },
+            local servers = {
+                "tsserver",
+                "html",
+                "cssls",
+                "jsonls",
+                "jdtls",
+                "lua_ls"
+            }
+
+            for _, server in ipairs(servers) do
+                lspconfig[server].setup({
+                    capabilities = capabilities,
+                    on_attach = function(client, bufnr)
+                        vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+                        local opts = { noremap = true, silent = true }
+                        vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+                        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>gd', '<cmd>lua vim.lsp.buf.definition()<CR>',
+                            opts)
+                        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>gr', '<cmd>lua vim.lsp.buf.references()<CR>',
+                            opts)
+                        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>',
+                            opts)
+
+                        if client.server_capabilities.documentFormattingProvider then
+                            print("Client Loaded Successfully.")
+                        else
+                            print("client.resolve_capa errors")
+                        end
+                    end,
+                })
+            end
+        end,
+    },
 }
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,22 +1,41 @@
 return {
     "nvimtools/none-ls.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "williamboman/mason.nvim",
+        "jay-babu/mason-null-ls.nvim"
+    },
     config = function()
         local null_ls = require('null-ls')
 
         null_ls.setup({
             sources = {
                 null_ls.builtins.formatting.prettier,
+                null_ls.builtins.diagnostics.stylelint,
+                null_ls.builtins.diagnostics.checkstyle.with({
+                    extra_args = { "-c", "/google_checks.xml" },
+                })
             },
             on_attach = function(client, bufnr)
-                if client.resolved_capabilities and client.resolved_capabilities.document_formatting then
+                if client.server_capabilities.documentFormattingProvider then
                     vim.cmd [[
                         augroup LspFormatting
                             autocmd! * <buffer>
-                            autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
+                            autocmd BufWritePre <buffer> lua vim.lsp.buf.format({ async = true })
                         augroup END
                     ]]
                 end
             end,
+        })
+
+        require("mason-null-ls").setup({
+            ensure_installed = {
+                "prettier",
+                "stylelint",
+                "markdownlint",
+                "checkstyle",
+            },
+            automatic_installation = true,
         })
     end
 }
